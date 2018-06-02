962c3633e1d662ffc1401403a36f0fd5cfe09259
Jed Lin <jed1978@gmail.com>
2018-06-02 18:26:56 +0800

    Refactor code structure

--- HumanTimeFormat/TimeFormat.cs
+++ HumanTimeFormat/TimeFormat.cs
@@ -9,7 +9,7 @@ namespace HumanTimeFormat
             if (s <= 0) return "now";
             if (s <= 59 && s >= 1)
             {
-                return FormatSeconds(s);
+                return ProcessTimeFormat(s, "second");
             }
 
             var format = "";
@@ -20,33 +20,25 @@ namespace HumanTimeFormat
             {
                 minute = minute % 60;
                 second = minute % 60;
-
                 format = ProcessTimeFormat(hour, "hour");
             }
 
             if (minute <= 59 && minute >= 1)
             {
-                format = $"{minute} minute";
-                format = FormatPlural(minute, format);
+                format = ProcessTimeFormat(minute, "minute");
             }
 
             if (second > 0)
             {
-                format = format + " and " + FormatSeconds(second);
+                format = format + " and " + ProcessTimeFormat(second, "second");
             }
 
             return format;
         }
 
-        private static string ProcessTimeFormat(int hour, string timeUnit)
+        private static string ProcessTimeFormat(int timeValue, string timeUnit)
         {
-            return FormatPlural(hour, $"{hour} {timeUnit}");
-        }
-
-        private string FormatSeconds(int s)
-        {
-            var format = $"{s} second";
-            return FormatPlural(s, format);
+            return FormatPlural(timeValue, $"{timeValue} {timeUnit}");
         }
 
         private static string FormatPlural(int s, string format)
