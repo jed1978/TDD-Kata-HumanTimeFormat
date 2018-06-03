79a8150ca6888f02be63c87adb17c58c2ea8b3af
Jed Lin <jed1978@gmail.com>
2018-06-03 22:00:03 +0800

    Refactor test case

--- HumanTimeFormat.UnitTests/HumanTimeFormatTests.cs
+++ HumanTimeFormat.UnitTests/HumanTimeFormatTests.cs
@@ -11,22 +11,31 @@ namespace HumanTimeFormat.UnitTests
     [TestFixture]
     public class HumanTimeFormatTests
     {
+        private TimeFormat _humanTimeFormat;
+
+        private void ShouldBe(int duration, string expected)
+        {
+            _humanTimeFormat.FormatDuration(duration).ShouldBeEqualTo(expected);
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _humanTimeFormat = new TimeFormat();
+        }
+
         [Test]
         public void Test_0_Return_now()
         {
             var expected = "now";
-            var humanTimeFormat = new TimeFormat();
-            var actual = humanTimeFormat.FormatDuration(0);
-            actual.ShouldBeEqualTo(expected);
+            ShouldBe(0, expected);
         }
 
         [Test]
         public void Test_1_Return_1_second()
         {
             var expected = "1 second";
-            var humanTimeFormat = new TimeFormat();
-            var actual = humanTimeFormat.FormatDuration(1);
-            actual.ShouldBeEqualTo(expected);
+            ShouldBe(1, expected);
         }
     }
 }
